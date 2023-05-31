cf2626d5ec3aed82f1b16d4b2ac8bc096d379077
Nico Greuel <greuelnico@gmail.com>
2023-05-31 10:41:53 +0200

    add BACKEND_BASE_URL as env var and improve mock data

--- setup/mock_data.py
+++ setup/mock_data.py
@@ -2,19 +2,10 @@ import random
 import string
 import psycopg2
 from datetime import date, timedelta
+from faker import Faker
 
-# Generate a random string of given length
-def generate_random_string(length):
-    letters = string.ascii_lowercase
-    return ''.join(random.choice(letters) for _ in range(length))
-
-# Generate a random date within a range
-def generate_random_date(start_date, end_date):
-    time_between = end_date - start_date
-    days_between = time_between.days
-    random_days = random.randrange(days_between)
-    random_date = start_date + timedelta(days=random_days)
-    return random_date
+# Create an instance of the Faker generator
+fake = Faker()
 
 # Connect to the PostgreSQL database
 conn = psycopg2.connect(
@@ -45,14 +36,13 @@ start_date = date(2023, 1, 1)
 end_date = date.today()
 
 for _ in range(10):
-    repo_name = generate_random_string(10)
-    workflow_name = generate_random_string(8)
-    run_id = random.randint(1000, 9999)
-    job_id = random.randint(1, 100)
-    status = random.choice(['queued', 'in_progress', 'completed', 'waiting'])
-    labels = generate_random_string(5)
-    event_type = status
-    created_at = generate_random_date(start_date, end_date)
+    repo_name = f'{fake.name().replace(" ", "-")}-{fake.company_suffix().replace(" ", "-")}/{fake.slug()}'
+    workflow_name = fake.bs().replace(" ", "-")
+    job_id = fake.random_int(min=1, max=100000)
+    run_id = fake.random_int(min=1, max=100000)
+    labels = ', '.join([f'{label}-{fake.random_element(elements=("runner", "hosted"))}' for label in fake.words(nb=3)])
+    event_type = random.choice(['queued', 'in_progress', 'completed', 'waiting'])
+    created_at = fake.date_time_this_year()
 
     query = "INSERT INTO workflow_runs (repo_name, workflow_name, run_id, job_id, labels, event_type, created_at) VALUES (%s, %s, %s, %s, %s, %s, %s)"
     values = (repo_name, workflow_name, run_id, job_id, labels, event_type, created_at)
